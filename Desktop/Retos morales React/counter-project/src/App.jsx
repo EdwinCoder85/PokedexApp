f1cf6cc4441e18eac480a02ea9658d5ace01d392
Edwin Amaranto <63481204+Eamaranto17@users.noreply.github.com>
2023-07-11 20:20:59 -0500

    commit 11jul23

--- Desktop/Retos morales React/counter-project/src/App.jsx
+++ Desktop/Retos morales React/counter-project/src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
-import Counter from './components/Counter'
 
-function App() {
-
-  return (
-    <>
-      <Counter />
-    </>
-  )
+import React from 'react';
+import "./App.css";
+import { useCounter } from 'hooks';
+  
+const App = () => {
+    const { value, increment, decrement, incrementBy, 
+        decrementBy, reset } = useCounter(1000);
+    return (
+        <div>
+            <p> Counter</p>
+            <p> <span>Current value </span>is {value}</p>
+            <button onClick={increment}>
+                Increment</button><br /><br />
+            <button onClick={decrement}>
+                Decrement</button><br /><br />
+            <button onClick={() => incrementBy(2)}>
+                Increase by 2</button><br /><br />
+            <button onClick={() => decrementBy(2)}>
+                Decrease by 2</button><br /><br />
+            <button onClick={reset}>reset</button>
+        </div>
+    )
 }
-
+  
 export default App
